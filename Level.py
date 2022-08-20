898cb63004e40c1e6e4aafa382620ebee06ed211
hanzili <hanzi.li@mail.mcgill.ca>
2022-08-20 05:02:46 -0400

    commit message

---
+++ Level.py
@@ -0,0 +1,28 @@
+from godot import exposed, export
+from godot import *
+from qiskit import *
+
+@exposed
+class Level(Spatial):
+
+	# member variables here, example:
+	a = export(int)
+	b = export(str, default='foo')
+
+	def _ready(self):
+		"""
+		Called every time the node is added to the scene.
+		Initialization here.
+		"""
+		circ = QuantumCircuit(3)
+		# Add a H gate on qubit 0, putting this qubit in superposition.
+		circ.h(0)
+		# Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
+		# the qubits in a Bell state.
+		circ.cx(0, 1)
+		# Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
+		# the qubits in a GHZ state.
+		circ.cx(0, 2)
+		print(circ)
+		print("Hello World")
+		pass
---
+++ scripts/button_title_play.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_button_title_play_pressed():
+	get_tree().change_scene("res://Level.tscn")
